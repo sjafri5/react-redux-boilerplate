170b59c9a882c9ef9738d2ade9cf90d86ab07797
sjafri5 <raza.jafri.sy@gmail.com>
2018-07-21 13:10:05 -0500

    second story answers

--- src/js/common/components/Utilities/pdfMaker.js
+++ src/js/common/components/Utilities/pdfMaker.js
@@ -12,27 +12,52 @@ class PdfMaker {
 
   buildDocument(){
     const date = Date.now()
+    this.doc.setFont('Times')
 
+    this.transcribeDate()
     this.transcribeQuestions()
     this.doc.save(date + '.pdf');
   }
 
+  transcribeDate(){
+    const date = new Date().toLocaleDateString("en-US")
+    this.doc.setFontSize(12)
+    this.doc.text("Date of Service: " + date, 10, 10);
+  }
+
   transcribeQuestions(){
     const questionCount = Array.from(Array(26)).map((e,i)=>(i+ 1).toString())
+    let yAxis = 20
     questionCount.map((questionNumber, index) => {
-      const yAxis = 10 + (index * 20)
       this.doc.setFontSize(12)
+      if(questionNumber === '2'){
+        this.doc.text('I. MENTAL STATUS ASSESSMENT', 10, yAxis);
+        yAxis += 8
+      }
       this.doc.text(this.questionMap[questionNumber].question, 10, yAxis);
       this.transcribeAnswers(questionNumber, yAxis)
+        if (QuestionMap[questionNumber].answers.length > 6) {
+          yAxis += 20;
+        } else {
+          yAxis += 15;
+        }
     })
   }
 
   transcribeAnswers(questionNumber, yAxis){
+    const zeAnswers = QuestionMap[questionNumber].answers
+    let xAxis = 10
     QuestionMap[questionNumber].answers.map((answer, index) => {
       const fillStyle = answer === this.formData.get(questionNumber) ? 'F': 'S' 
-      this.doc.rect((10 + (index * 25)), yAxis + 5, 2, 2, fillStyle)
+      this.doc.rect(xAxis, yAxis + 5, 2, 2, fillStyle)
       this.doc.setFontSize(8)
-      this.doc.text(answer, (13 + (index * 25)), yAxis + 7)
+      this.doc.text(answer, xAxis + 3, yAxis + 7)
+      if (index === 5){
+        xAxis = 10
+        yAxis += 5
+      } else {
+        xAxis += 25
+      }
     })
   }
 }
